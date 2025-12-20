3df41268624b274941260d1b3d9b787a717b3359
BrendanKechtban <147231727+BrendanKechtban@users.noreply.github.com>
2025-12-19 23:24:20 -0600

    Update train.py
    Removed comment about loading and preprocessing the dataset.

--- train.py
+++ train.py
@@ -4,7 +4,6 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from tensorflow.keras.datasets import fashion_mnist
 import os
 
-# Load and preprocess the dataset
 (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
