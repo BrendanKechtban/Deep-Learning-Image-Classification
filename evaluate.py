caf9541e914e5c6f2f0d32d2b882b6bb9da2688f
BrendanKechtban <147231727+BrendanKechtban@users.noreply.github.com>
2026-01-05 02:31:45 -0600

    Update evaluate.py
    Add confusion matrix visualization for predictions

--- evaluate.py
+++ evaluate.py
@@ -19,7 +19,7 @@ y_pred = model.predict(x_test)
 y_pred_classes = np.argmax(y_pred, axis=1)
 y_true = np.argmax(y_test, axis=1)
 
-# Confusion matrix
+
 cm = confusion_matrix(y_true, y_pred_classes)
 plt.figure(figsize=(10, 8))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=range(10), yticklabels=range(10))
